f505646120df9198fc2bac79d274715bc2ad2564
bpo106 <bpo106bkv@gmail.com>
2018-04-21 12:20:35 +0200

    UIC Calculator

---
+++ UICCalculator/UICCalculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UICCalculator
+{
+    class Program
+    {
+        static int Uic(string s)
+        {
+            int checkDigit = 0;
+            while(s.Length > 0)
+            {
+                int num = s[0] - 48;
+                if (s.Length % 2 == 1)
+                {
+                    num *= 2;
+                }
+                checkDigit += (num < 10) ? num : num / 10 + num % 10;
+                s = (s.Length == 1) ? "" : s.Substring(1, s.Length - 1);
+            }
+            return 10 - (checkDigit % 10);
+        }
+
+        static bool IsCodeRight (string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] > 57 || s[i] < 48)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "";
+            while (s.Length != 11 || !IsCodeRight(s))
+            {
+                Console.WriteLine("Type the numbers in without the check digit:");
+                s = Console.ReadLine();
+                if (s.Length != 11 || !IsCodeRight(s))
+                {
+                    Console.WriteLine("Wrong code. Try again.");
+                }
+            }
+            int checkDigit = Uic(s);
+            Console.WriteLine("The check digit is {0}.", checkDigit);
+            Console.ReadLine();
+        }
+    }
+}
